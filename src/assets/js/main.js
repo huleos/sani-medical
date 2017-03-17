57fef6992fc74071b65fae3346c2c214feea43ba
Julio Cesar Montenegro <creativo@686studio.com>
2017-03-17 15:23:27 -0700

    fixed modal

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,22 +4,34 @@ $(function() {
 	///////////
 
 	// Open
-	$('[modal-open]').on('click', function(e)  {
+	// $('[modal-open]').on('click', function(e)  {
+	// 	var targeted_modal_class = $(this).attr('modal-open');
+	// 	$('[modal="' + targeted_modal_class + '"]').fadeIn(350);
+	// 	e.preventDefault();
+	// });
+
+	// Close
+	// $('[modal-close], .modal').on('click', function(e)  {
+	// 	var targeted_modal_class = $(this).attr('modal-close');
+	// 	$('[modal="' + targeted_modal_class + '"]').fadeOut(350);
+	// 	e.preventDefault();
+	// });
+	
+	// Open
+	$('[modal-open]').click(function()  {
 		var targeted_modal_class = $(this).attr('modal-open');
 		$('[modal="' + targeted_modal_class + '"]').fadeIn(350);
-		e.preventDefault();
 	});
 
 	// Close
-	$('[modal-close], .modal').on('click', function(e)  {
+	$('[modal-close]').click(function()  {
 		var targeted_modal_class = $(this).attr('modal-close');
 		$('[modal="' + targeted_modal_class + '"]').fadeOut(350);
-		e.preventDefault();
 	});
 
-	// $('.modal').click(function() {
-	// 	$(this).parent().fadeOut(350);
-	// });
+	$('.modal, .modal-container').click(function() {
+		$(this).fadeOut(350);
+	});
 
 	// Form Validation
 	$('form').validate();
