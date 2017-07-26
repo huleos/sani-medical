7d2ace0e694e0cca0592f609636c2b7a54ea22fc
Julio Cesar Montenegro <creativo@686studio.com>
2017-07-25 17:16:35 -0700

    added email

--- dist/events_form.php
+++ dist/events_form.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-$myemail = '';
+$myemail = 'genesis@sanimedicaltourism.com';
 $bccmail = 'creativo@686studio.com';
 $noreply = 'noreply@sanimedicaltourism.com';
 
